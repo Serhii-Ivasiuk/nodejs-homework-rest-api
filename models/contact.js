36d943432f2341175dc7c9370089e69d0a04550c
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-07-15 18:54:31 +0300

    added handleMongooseError helper for changing status from 500 to 400 of mongoose validation errors

--- models/contact.js
+++ models/contact.js
@@ -1,9 +1,13 @@
 // Libs
 const { Schema, model } = require('mongoose');
+// Helpers
+const handleMongooseError = require('../helpers/handleMongooseError');
 // Modules
 const { mongooseSchema } = require('../schemas/contacts');
 
 const contactSchema = new Schema(...mongooseSchema);
+
+contactSchema.post('save', handleMongooseError);
 
 const Contact = model('contact', contactSchema);
 
