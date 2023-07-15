c64b7c605915068c9372fc1e154bd159284a70c2
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-07-15 20:23:24 +0300

    add checking dublicate before adding new contact

--- helpers/handleMongooseError.js
+++ helpers/handleMongooseError.js
@@ -1,5 +1,12 @@
 const handleMongooseError = (error, data, next) => {
+    if (error.code === 11000) {
+        const [field, value] = Object.entries(error.keyValue)[0];
+
+        error.message = `Contact with "${field}" "${value}" is already exist`;
+    }
+
     error.status = 400;
+
     next();
 };
 
