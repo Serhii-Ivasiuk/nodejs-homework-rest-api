6c21657a7d61fb95effa0087ac4beae5189fdbc5
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-07-15 20:42:12 +0300

    Update isValidId.js

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -7,7 +7,7 @@ const isValidId = (req, res, next) => {
     const { contactId } = req.params;
 
     if (!isValidObjectId(contactId))
-        next(HttpError(400, `"${contactId}" is not valid id`));
+        next(HttpError(400, `Id "${contactId}" is not valid id`));
 
     next();
 };
