3413d49454498b18ffd46492c98d76a9642e3df0
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-07-26 19:55:32 +0300

    added filtration contacts collection by favorite field

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -4,10 +4,20 @@ const { Contact } = require('../../models');
 const getAllContacts = async (req, res) => {
     const { _id: owner } = req.user;
 
-    const result = await Contact.find({ owner }).populate(
-        'owner',
-        'email subscription'
-    );
+    const { page = 1, limit = 20, favorite } = req.query;
+
+    const skip = (page - 1) * limit;
+
+    const filter = favorite ? { favorite } : {};
+
+    const result = await Contact.find(
+        { owner, ...filter },
+        {},
+        {
+            skip,
+            limit,
+        }
+    ).populate('owner', 'email subscription');
 
     res.json(result);
 };
