f841d2b1f32b4f3b94ecfebf2d312145b434d611
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-07-30 14:59:47 +0300

    bugfix - allow users to add the same contacts

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -54,17 +54,14 @@ const schema = {
     name: {
         type: String,
         required: [true, 'Set "name" for contact'],
-        unique: true,
     },
     email: {
         type: String,
         required: [true, 'Set "email" for contact'],
-        unique: true,
     },
     phone: {
         type: String,
         required: [true, 'Set "phone" for contact'],
-        unique: true,
     },
     favorite: {
         type: Boolean,
